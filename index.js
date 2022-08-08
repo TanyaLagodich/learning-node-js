1e67e0b99e41a56a7929191fc87110cb446a3a98
Tanya Lagodich <tanyalagodichka@gmail.com>
2022-08-08 19:15:03 +0300

    learning nodejs

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const userName = 'Tanya';
+import fs from 'fs/promises';
+import _ from 'lodash';
 
-const sayHi = (userName) => `Hello, my name is ${userName}`;
+import makeServer from './server.js';
 
-module.exports = {
-    userName,
-    sayHi,
+export default async (port, callback = () => {}) => {
+  const data = await fs.readFile('phonebook.txt', 'utf-8');
+
+  // BEGIN (write your solution here)
+  // console.log(data.split('\n').map(str => str.split(' | ')));
+  const usersById = 1;
+  // END
+
+  const server = makeServer(usersById);
+  server.listen(port, () => callback(server));
 };
--- modules.js
+++
@@ -1,6 +0,0 @@
-const os = require('os');
-const { userName: user, sayHi } = require('./index');
-
-const name = 'Tommy';
-
-console.log(os.platform(), os.release());
